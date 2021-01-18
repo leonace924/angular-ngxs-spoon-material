55e2e2a70fff72b16da7e112e1635fc212bc78c4
aceberry924 <meobius123@gmail.com>
2021-01-18 21:56:03 +0800

    add ngxs-labs and update README.md

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 import { NgxsModule } from '@ngxs/store';
+import { NgxsActionsExecutingModule } from '@ngxs-labs/actions-executing';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -9,12 +11,14 @@ import { HomeComponent } from './pages/home/home.component';
 import { DetailsComponent } from './pages/details/details.component';
 
 import { MaterialModule } from './material.module';
+import { ServicesModule } from './services/services.module';
 
 import { environment as env } from 'src/environments/environment';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchBarComponent } from './components/search-bar/search-bar.component';
 
-
+import { MenuItemState } from './states/menu-item.state';
+import { MenuCardComponent } from './components/menu-card/menu-card.component';
 @NgModule({
   declarations: [
     AppComponent,
@@ -22,13 +26,17 @@ import { SearchBarComponent } from './components/search-bar/search-bar.component
     DetailsComponent,
     HeaderComponent,
     SearchBarComponent,
+    MenuCardComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    NgxsModule.forRoot([], { developmentMode: !env.production }),
-    MaterialModule
+    HttpClientModule,
+    MaterialModule,
+    ServicesModule,
+    NgxsModule.forRoot([MenuItemState], { developmentMode: !env.production }),
+    NgxsActionsExecutingModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
